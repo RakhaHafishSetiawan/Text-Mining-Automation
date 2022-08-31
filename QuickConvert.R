83a80c58330ddaed9eed41afad32ff185f83398c
Rakha Hafish Setiawan <102712597+RakhaHafishSetiawan@users.noreply.github.com>
2022-08-31 10:46:50 +0700

    First Commit

---
+++ QuickConvert.R
@@ -0,0 +1,22 @@
+QuickConvert = function(Location) {
+  
+  #Check Library
+  if (!require("tm")) 
+    install.packages("tm")
+  
+  if (!require("pdftools")) 
+    install.packages("pdftools")
+  
+  #Activate Library
+  library(tm)
+  library(pdftools)
+  
+  #Insert Location
+  text <- pdf_text(Location)
+  
+  #Create Corpus
+  textCorpus <<- Corpus(VectorSource(text))}
+
+
+
+
